d9a874de36421587b9c2a24deccb4943885eb7b0
Marcelino <marcelinocost69@gmail.com>
2022-09-07 21:16:30 -0300

    correção no Projeto

--- routes/teste.js
+++ routes/teste.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("teste", { title: "Node é Super Show" })
+  res.render("teste", { title: "Node é Super Show", numero: 20 })
 })
 
 module.exports = router
